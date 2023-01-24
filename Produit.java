643a5d7121b616c2b0563b63551d59144daad2ba
KarlHarnois <karlrivestharnois@gmail.com>
2023-01-24 17:53:13 -0500

    Remove/Add linebreaks in `Product` class
    Small refactoring to cleanup the `Product` class & make linebreaks
    consistent.

--- Produit.java
+++ Produit.java
@@ -1,12 +1,10 @@
 public class Produit {
-
     protected String id;
     protected String nom;
     protected double prixAchat;
     protected double prixVente;
 
     public Produit(String id, String nom, double prixAchat, double prixVente){
-
         this.id=id;
         this.nom=nom;
         this.prixAchat=prixAchat;
@@ -16,9 +14,11 @@ public class Produit {
     public double reductionPrix(int reduction){
         return prixVente=prixVente*reduction;
     }
+
     public double augmenterPrix(int augmentation){
         return prixVente=prixVente*(1+augmentation/100);
     }
+
     public String toString() {
         return "Produit " +" iD: " + id + " nom: " + nom + " prixAchat: " + prixAchat + " prixVente: " + prixVente;
     }
